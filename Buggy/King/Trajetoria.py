cdb23b1738d407f85e06dc8806515e175d4037b4
kelvinludwig <kelvinludwig@roadseg.com.br>
2021-07-15 19:47:50 -0300

    pastas pra teste e arquivos pra teste

---
+++ Buggy/King/Trajetoria.py
@@ -0,0 +1,9 @@
+Trajetoria Buggy
+
+    1) Buggy deixa a tripulação do Rei dos Piratas (Gol D. Roger).
+
+    2) Buggy monta a tripulação.
+
+    3) Buggy invade Orange Town.
+
+    4) Buggy apanha para Luffy.
---
+++ Luffy/Zoro/tr.js
@@ -0,0 +1,5 @@
+Trajetória Luffy
+ - Luffy Nasce
+ - Luffy Conhece Ace
+ - Luffy Sai pra o mar
+ - Luffy Encontra buggy e derrota-o
